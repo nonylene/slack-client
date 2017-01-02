57543c680d2d9cdb7d0ef57a765135bba2a48bb0
nonylene <cookie.speaker@gmail.com>
2017-01-02 20:14:23 +0900

    channel watching

--- slack-client.py
+++ slack-client.py
@@ -8,6 +8,7 @@ import time
 import config
 
 interrupted = False
+channels = dict()
 
 def on_open(ws):
     message = "connection opened!"
@@ -37,6 +38,7 @@ def on_error(ws, error):
 def on_message(ws, message):
     message_data = json.loads(message)
     emoji_watch(message_data)
+    channel_watch(message_data)
 
 # client features below here
 def emoji_watch(data):
@@ -58,9 +60,76 @@ def emoji_watch(data):
         message = "{0} :{1}: ({1}) {2}!".format(prefix_emoji, emoji, data["subtype"])
         post_message(config.EMOJI_WATCH_CHANNEL, message, username = "Emoji Watcher", icon_emoji = icon_emoji)
 
+def channel_watch(data):
+
+    def channel_link(channel_id):
+        return '<#{0}|{1}>'.format(channel_id, channels[channel_id])
+
+    def post_channel_message(text):
+        post_message(
+                config.CHANNEL_WATCH_CHANNEL,
+                ":rooster: " + text,
+                'channel-bot',
+                ':tokyo_tower:'
+                )
+
+    # https://api.slack.com/events/channel_archive
+    # https://api.slack.com/events/channel_created
+    # https://api.slack.com/events/channel_deleted
+    # https://api.slack.com/events/channel_rename
+    # https://api.slack.com/events/channel_unarchive
+    if data['type'] == 'channel_archive':
+        post_channel_message(
+                'Archived: {0}'.format(channel_link(data['channel']))
+                )
+
+    elif data['type'] == 'channel_created':
+        channel_id = data['channel']['id']
+        channels[channel_id] = data['channel']['name']
+        post_channel_message(
+                'Created: {0}'.format(channel_link(channel_id))
+                )
+
+    elif data['type'] == 'channel_deleted':
+        channel_name = channels.pop(data['channel'])
+        post_channel_message(
+                'Deleted: #{0}'.format(channel_name)
+                )
+
+    elif data['type'] == 'channel_rename':
+        channel_id = data['channel']['id']
+        old_channel_name = channels[channel_id]
+        channels[channel_id] = data['channel']['name']
+        post_channel_message(
+                'Renamed: #{0} :arrow_right: {1}'.format(
+                    old_channel_name,
+                    channel_link(channel_id)
+                    )
+                )
+
+    elif data['type'] == 'channel_unarchive':
+        post_channel_message(
+                'Unarchived: {0}'.format(channel_link(data['channel']))
+                )
+
 def connect():
+    params = urllib.parse.urlencode({'token': config.TOKEN})
     try:
-        params = urllib.parse.urlencode({'token': config.TOKEN})
+        # initial -> get channel list
+        channels_api = "https://slack.com/api/channels.list?{0}".format(params)
+        res = urllib.request.urlopen(channels_api)
+        list_data = json.loads(res.read().decode())
+        global channels
+        channels = {c["id"]: c["name"] for c in list_data["channels"]}
+    except Exception as e:
+        traceback.print_exc()
+        post_message(config.DEBUG_CHANNEL,
+                "get channel list failed: {0}".format(e),
+                ":upside_down_face:"
+                )
+        raise
+
+    try:
         start_api = "https://slack.com/api/rtm.start?{0}".format(params)
         res = urllib.request.urlopen(start_api)
         start_data = json.loads(res.read().decode())
@@ -77,7 +146,11 @@ def connect():
 
     except Exception as e:
         traceback.print_exc()
-        post_message(config.DEBUG_CHANNEL, str(e), ":upside_down_face:")
+        post_message(config.DEBUG_CHANNEL,
+                "create websocket failed: {0}".format(e),
+                ":upside_down_face:"
+                )
+        raise
 
 def post_message(channel, text, username=config.DEFAULT_USERNAME, icon_emoji=":upside_down_face:"):
     data = {
