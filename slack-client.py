2607cdee4533057c61d5a2e9e8255165277c5ffb
nonylene <nonylene@gmail.com>
2020-02-16 03:16:00 +0900

    Use asyncio with httpx and websockets
    - Fix memory leak

--- slack-client.py
+++ slack-client.py
@@ -1,11 +1,15 @@
-import websocket
-import urllib.parse
-import urllib.request
 import json
 import traceback
 import time
-import click
+import asyncio
 from dataclasses import dataclass
+
+import websockets
+import websockets.exceptions
+import click
+import httpx
+
+client = httpx.AsyncClient()
 
 
 @dataclass
@@ -17,47 +21,34 @@ class Config:
     debug_channel: str
 
 
-interrupted = False
 channels = dict()
 config: Config = None
 
 
-def on_open(ws):
+async def notify_open():
     message = "Connection opened!"
     print(message)
-    # post slack
-    post_text(config.debug_channel, message)
+    # Post slack
+    await post_text(config.debug_channel, message)
 
 
-def on_close(ws, *close_args):
+async def notify_close():
     message = "Connection closed!"
     print(message)
-    # post slack error
-    post_text(config.debug_channel, message)
-    if not interrupted:
-        # retry
-        print("Reconnecting...")
-        time.sleep(2)
-        connect()
+    # post slack
+    await post_text(config.debug_channel, message)
 
 
-def on_error(ws, error):
-    traceback.print_exc()
-    # KeyboardInterrupt arrive here (0.37)
-    # SystemError not arrive here (0.37~)
-    if isinstance(error, SystemError) or isinstance(error, KeyboardInterrupt):
-        global interrupted
-        interrupted = True
-
-
-def on_message(ws, message):
+async def on_message(message):
     message_data = json.loads(message)
-    emoji_watch(message_data)
-    channel_watch(message_data)
+    await asyncio.gather(
+        emoji_watch(message_data),
+        channel_watch(message_data)
+    )
 
 
 # client features below here
-def emoji_watch(data):
+async def emoji_watch(data):
     # https://api.slack.com/events/emoji_changed
     if not "emoji_changed" == data["type"]:
         return
@@ -72,7 +63,7 @@ def emoji_watch(data):
         aliased = value.startswith('alias:')
         if aliased:
             message = f":raising_hand: Alias added: {emoji} (*{name}*, {value})"
-            post_text(
+            await post_text(
                 config.emoji_watch_channel, message,
                 "emoji-bot", emoji,
             )
@@ -96,7 +87,7 @@ def emoji_watch(data):
                 "text": message,
                 "blocks": [block],
             }
-            post_message(
+            await post_message(
                 config.emoji_watch_channel, data,
                 "emoji-bot", emoji
             )
@@ -105,19 +96,19 @@ def emoji_watch(data):
         # Remove
         for name in data["names"]:
             message = f":wave: Removed: :{name}: (*{name}*)"
-            post_text(
+            await post_text(
                 config.emoji_watch_channel, message,
                 "emoji-bot", ":upside_down_face:",
             )
 
 
-def channel_watch(data):
+async def channel_watch(data):
 
     def channel_link(channel_id):
         return '<#{0}|{1}>'.format(channel_id, channels[channel_id])
 
-    def post_channel_message(text):
-        post_text(
+    async def post_channel_message(text):
+        await post_text(
             config.channel_watch_channel,
             text,
             'channel-bot',
@@ -130,14 +121,14 @@ def channel_watch(data):
     # https://api.slack.com/events/channel_rename
     # https://api.slack.com/events/channel_unarchive
     if data['type'] == 'channel_archive':
-        post_channel_message(
+        await post_channel_message(
             ':ghost: Archived: {0}'.format(channel_link(data['channel']))
         )
 
     elif data['type'] == 'channel_created':
         channel_id = data['channel']['id']
         channels[channel_id] = data['channel']['name']
-        post_channel_message(
+        await post_channel_message(
             ':hatching_chick: Created: {0}'.format(
                 channel_link(channel_id)
             )
@@ -145,7 +136,7 @@ def channel_watch(data):
 
     elif data['type'] == 'channel_deleted':
         channel_name = channels.pop(data['channel'])
-        post_channel_message(
+        await post_channel_message(
             ':see_no_evil: Deleted: #{0}'.format(channel_name)
         )
 
@@ -154,28 +145,26 @@ def channel_watch(data):
         old_channel_name = channels[channel_id]
         new_channel_name = data['channel']['name']
         channels[channel_id] = new_channel_name
-        post_channel_message(
+        await post_channel_message(
             f':ocean: Renamed: {channel_link(channel_id)} (#{old_channel_name} :arrow_right: #{new_channel_name})'
         )
 
     elif data['type'] == 'channel_unarchive':
-        post_channel_message(
+        await post_channel_message(
             ':sushi: Unarchived: {0}'.format(channel_link(data['channel']))
         )
 
 
-def connect():
-    params = urllib.parse.urlencode({'token': config.token})
+async def connect():
+    token_params = {'token': config.token}
     try:
-        # initial -> get channel list
-        channels_api = "https://slack.com/api/channels.list?{0}".format(params)
-        res = urllib.request.urlopen(channels_api)
-        list_data = json.loads(res.read().decode())
+        # Initial -> get channel list
+        r = await client.get('https://slack.com/api/channels.list', params=token_params)
+        list_data = r.json()
         global channels
         channels = {c["id"]: c["name"] for c in list_data["channels"]}
     except Exception as e:
-        traceback.print_exc()
-        post_text(
+        await post_text(
             config.debug_channel,
             "get channel list failed: {0}".format(e),
             ":upside_down_face:"
@@ -183,24 +172,24 @@ def connect():
         raise
 
     try:
-        start_api = "https://slack.com/api/rtm.connect?{0}".format(params)
-        res = urllib.request.urlopen(start_api)
-        start_data = json.loads(res.read().decode())
-        websocket_url = start_data["url"]
-        # websocket.enableTrace(True)
-        ws = websocket.WebSocketApp(
-            websocket_url,
-            on_message=on_message,
-            on_error=on_error,
-            on_open=on_open,
-            on_close=on_close
-        )
-
-        ws.run_forever()
-
+        while True:
+            r = await client.get('https://slack.com/api/rtm.connect', params=token_params)
+            start_data = r.json()
+            websocket_url = start_data["url"]
+            try:
+                async with websockets.connect(websocket_url) as websocket:
+                    await notify_open()
+                    async for message in websocket:
+                        await on_message(message)
+            except websockets.exceptions.ConnectionClosed:
+                traceback.print_exc()
+                await notify_close()
+                print("Reconnecting...")
+                # Retry
+                time.sleep(2)
+                continue
     except Exception as e:
-        traceback.print_exc()
-        post_text(
+        await post_text(
             config.debug_channel,
             "create websocket failed: {0}".format(e),
             ":upside_down_face:"
@@ -208,16 +197,16 @@ def connect():
         raise
 
 
-def post_text(
+async def post_text(
         channel, text,
         username=None, icon_emoji=":upside_down_face:"
 ):
     if username is None:
         username = config.default_username
-    post_message(channel, {"text": text}, username, icon_emoji)
+    await post_message(channel, {"text": text}, username, icon_emoji)
 
 
-def post_message(
+async def post_message(
         channel, data_dict,
         username=None, icon_emoji=":upside_down_face:"
 ):
@@ -230,11 +219,7 @@ def post_message(
         "username": username
     }
     merged = {**data_dict, **base}
-    post_data = urllib.parse.urlencode(merged).encode()
-    urllib.request.urlopen(
-        "https://slack.com/api/chat.postMessage",
-        data=post_data
-    )
+    await client.post("https://slack.com/api/chat.postMessage", data=merged)
 
 
 @click.command()
@@ -250,7 +235,7 @@ def main(
     config = Config(
         token, default_username, emoji_watch_channel, channel_watch_channel, debug_channel
     )
-    connect()
+    asyncio.run(connect())
 
 
 if __name__ == "__main__":
